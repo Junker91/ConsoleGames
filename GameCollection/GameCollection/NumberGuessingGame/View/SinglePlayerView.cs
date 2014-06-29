9796c996055986922f1be0ffbd6afe23fe92f016
Xena92 <Ida.thuesen@hotmail.com>
2014-06-30 00:20:53 +0200

    Ting hænger sammen

--- GameCollection/GameCollection/NumberGuessingGame/View/SinglePlayerView.cs
+++ GameCollection/GameCollection/NumberGuessingGame/View/SinglePlayerView.cs
@@ -30,7 +30,7 @@ namespace GameCollection.NumberGuessingGame.View {
             name = Console.ReadLine();
             
             do{
-                Console.WriteLine("Enter the lowest and higehst possible number for your game seperated by space: ");
+                Console.WriteLine("Enter the lowest and higehst possible number for your game seperated by enter: ");
                 checkLow = TryCatch(ref lowestNumber);
                 checkHigh = TryCatch(ref highestNumber);
             }while(!checkLow || !checkHigh);  
@@ -45,7 +45,35 @@ namespace GameCollection.NumberGuessingGame.View {
         }
 
         private void SinglePlayerGame() {
+            int guess;
 
+            Console.WriteLine("Let the game begin!!. You have {0} lives to guess the number and an interval {1} ", _controller.PlayerLives(), _controller.UserInterval());
+
+            do {
+                Console.WriteLine("Take a guess:");
+                guess = int.Parse(Console.ReadLine());
+
+                do{
+                    if (_controller.UserGuessValid(guess))
+                        Console.WriteLine("Invalid number");
+
+                }while(!_controller.UserGuessValid(guess));
+
+                if (_controller.UserGuessPosition(guess) == 1)
+                    Console.WriteLine("Guess must be smaller than {0}", guess);
+                else if (_controller.UserGuessPosition(guess) == -1)
+                    Console.WriteLine("Guess must be higher than {0}", guess);
+
+            } while (_controller.IsUserStillAlive() && _controller.UserGuessPosition(guess) != 0);
+
+            if (_controller.IsUserStillAlive())
+                Console.WriteLine("You won the game!");
+            else
+                Console.WriteLine("You lost. Better luck next time");
+
+
+            Console.WriteLine("Press any key to return to main menu");
+            Console.ReadKey();
         }
 
         private bool TryCatch(ref int number) {
